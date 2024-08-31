639a4b7e88f4c7921f8336b005875f8264726b8a
inavicat <inavicat@gmail.com>
2024-08-31 14:25:18 +0700

    new UI

--- app.vue
+++ app.vue
@@ -1,37 +1,74 @@
 <template>
-    <div>
+  <div class="q-pa-md q-gutter-sm">
+    <q-bar>
+      <q-btn dense flat :icon="fabApple" />
+      <div class="text-weight-bold">App</div>
+      <div class="cursor-pointer gt-md">File</div>
+      <div class="cursor-pointer gt-md">Edit</div>
+      <div class="cursor-pointer gt-md">View</div>
+      <div class="cursor-pointer gt-md">Window</div>
+      <div class="cursor-pointer gt-md">Help</div>
+      <q-space />
+      <q-btn dense flat icon="airplay" class="gt-xs" />
+      <q-btn dense flat icon="battery_charging_full" />
+      <q-btn dense flat icon="wifi" />
+      <div>9:41</div>
+      <q-btn dense flat icon="search" />
+      <q-btn dense flat icon="list" />
+    </q-bar>
 
-      <QBtn color="primary" label="Primary" />
-      <QBtn @click="fullHeight=true" color="secondary" label="Secondary" />
-      <QBtn @click="fullHeight=true" color="secondary" label="Secondary TEST ss" />
+    <q-bar class="bg-black text-white">
+      <q-btn dense flat :icon="fabApple" />
+      <div class="text-weight-bold">App</div>
+      <div class="cursor-pointer gt-md">File</div>
+      <div class="cursor-pointer gt-md">Edit</div>
+      <div class="cursor-pointer gt-md">View</div>
+      <div class="cursor-pointer gt-md">Window</div>
+      <div class="cursor-pointer gt-md">Help</div>
+      <q-space />
+      <q-btn dense flat icon="airplay" class="gt-xs" />
+      <q-btn dense flat icon="battery_charging_full" />
+      <q-btn dense flat icon="wifi" />
+      <div>9:41</div>
+      <q-btn dense flat icon="search" />
+      <q-btn dense flat icon="list" />
+    </q-bar>
 
+    <q-bar>
+      <q-btn dense flat round icon="lens" size="8.5px" color="red" />
+      <q-btn dense flat round icon="lens" size="8.5px" color="yellow" />
+      <q-btn dense flat round icon="lens" size="8.5px" color="green" />
+      <div class="col text-center text-weight-bold">My-App</div>
+    </q-bar>
+
+    <q-bar dark class="bg-primary text-white">
+      <q-btn dense flat round icon="lens" size="8.5px" color="red" />
+      <q-btn dense flat round icon="lens" size="8.5px" color="yellow" />
+      <q-btn dense flat round icon="lens" size="8.5px" color="green" />
+      <div class="col text-center text-weight-bold">My-App</div>
+    </q-bar>
   </div>
 
-  <q-dialog
-      v-model="fullHeight"
-      full-height
-    >
-      <q-card class="column full-height" style="width: 300px">
-        <q-card-section>
-          <div class="text-h6">Full Height</div>
-        </q-card-section>
+  <q-dialog v-model="fullHeight" full-height>
+    <q-card class="column full-height" style="width: 300px">
+      <q-card-section>
+        <div class="text-h6">Full Height</div>
+      </q-card-section>
 
-        <q-card-section class="col q-pt-none">
-          Click/Tap on the backdrop.
-          Test
-        </q-card-section>
-        <q-card-section class="col q-pt-none">
-          Click/Tap on the backdrop.
-          Test
-        </q-card-section>
+      <q-card-section class="col q-pt-none">
+        Click/Tap on the backdrop. Test
+      </q-card-section>
+      <q-card-section class="col q-pt-none">
+        Click/Tap on the backdrop. Test
+      </q-card-section>
 
-        <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="OK" v-close-popup />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+      <q-card-actions align="right" class="bg-white text-teal">
+        <q-btn flat label="OK" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 
-    <div class="q-pa-md q-gutter-sm">
+  <div class="q-pa-md q-gutter-sm">
     <q-btn color="white" text-color="black" label="Standard" />
     <q-btn color="primary" label="Primary" />
     <q-btn color="secondary" label="Secondary" />
@@ -44,12 +81,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const fullHeight = ref(false)
+const fullHeight = ref(false);
 
 onMounted(() => {
-  console.log('mounted')
-})
-
+  console.log("mounted");
+});
 </script>
